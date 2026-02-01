7fc36865cdea8ff59f37c5bddc41a5279a3fa393
Jose Luis Yana <joseyananicolas@gmail.com>
2026-02-01 04:05:44 -0500

    feat: ajustes cokkies

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,11 +1,13 @@
 import { Response } from "express";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const setSessionCookie = (res: Response, token: string, expiresAt: Date) => {
   res.cookie("session-token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
-    expires: expiresAt,
-    path: "/",
+      httpOnly: true,
+      secure: isProduction, 
+      sameSite: isProduction ? "none" : "lax", 
+      path: "/",
+      expires: expiresAt,
   });
 };
